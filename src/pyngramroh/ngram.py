f51ac1c2aba530f435e561f3e8a953203f7f73b0
IT-Administrators <91905626+IT-Administrators@users.noreply.github.com>
2025-12-19 14:08:25 +0100

    Changed the ngram package
    Changed the ngram package to support words and character
    groups.

--- src/pyngramroh/ngram.py
+++ src/pyngramroh/ngram.py
@@ -2,107 +2,66 @@
 Pyngramroh.ngram
 
 Create an ngram of the provided string.
-
-Usage:
-    from pyngramroh import ngram as ng
-    ngram = ng.NGram("This is a demo string.", 2)
-    print(ngram.generate_ngram())
-
-Returns:
-    [['This', 'is'], ['is', 'demonstration'], ['demonstration', 'string']]
-
-Author: IT-Administrators
-
-License: MIT
 """
-
-import string
 
 class NGram:
     """NGram class."""
 
-    def __init__(self, source:str, ngramcount = 1):
+    def __init__(self, source:str, ngramcount:int = 1, ignore_words:bool = False, delimiter:str = " ", blank_replacement = "_"):
         self.source = source
         self.ngramcount = ngramcount
+        self.ignore_words = ignore_words
+        self.delimiter = delimiter
+        self.blank_replacement = blank_replacement
 
-    def generate_ngram(self):
+    def generate_ngram(self) -> list[str]:
         """
         Generate ngrams of the specified text using the ngramcount. 
-        The ngramcount is the number of words which will be grouped together.
-        The result is a list of lists. 
-        
-        Usage:
-            ngram = ng.NGram("This is a demonstration string.")
-            print(ngram.generate_ngram(words, 2))
-            print(ngram.generate_ngram(words, 3))
-            print(ngram.generate_ngram(words, 4))
-        
-        Return:
-            [['This', 'is'], ['is', 'a'], ['a', 'demo'], ['demo', 'string']]
-            [['This', 'is', 'a'], ['is', 'a', 'demo'], ['a', 'demo', 'string']]
-            [['This', 'is', 'a', 'demo'], ['is', 'a', 'demo', 'string']]
-        
-        Author: IT-Administrators
-        
-        License: MIT
-        """
-
-        # Result should be [[list1],[list2],[list3]]
-        # Result list.
-        ngramr = []
-        # Sublists.
-        ngramg = []
-        counter = 0
-        
-        # Create list of words to process.
-        self.words = self._split_string_to_words()
-
-        while counter < len(self._split_string_to_words()):
-            # Append word to sublist.
-            ngramg.append(self.words[counter])
-            if len(ngramg) == self.ngramcount:
-                # Append sublist to result list.
-                ngramr.append(ngramg)
-                # Remove first word from sublist.
-                ngramg = ngramg[1:]
-            counter += 1
-
-        return ngramr
-    
-    def _split_string_to_words(self):
-        """
-        Split string into words. This function is only meant to be a helper function.
-        
-        Usage:
-            teststring = "This is a demo string."
-            print(_split_string_to_words(teststring))
-        
-        Return:
-            ['This', 'is', 'a', 'demo', 'string.']
-        
-        Author: IT-Administrators
-        
-        License: MIT
-        """
-        # Remove punctuation than split to words.
-        return self._remove_punctuation().split(' ')
-
-    
-    def _remove_punctuation(self):
-        """
-        Remove every punctuation character in specified string.
-        
-        This function is a helpfer function. Don't import it.
+        The ngramcount is the number of words/characters which will be grouped together.
+        The result is a list of strings. 
 
         Usage:
-            print(_remove_punctuation("Test."))
-        
-        Result:
-            Test
+            from pyngramroh import ngram as ng
+            ngram = ng.NGram("This is a demo string", 2)
+            print(ngram.generate_ngram())
+
+        Returns:
+            ['This_is', 'is_a', 'a_demo', 'demo_string']
 
         Author: IT-Administrators
-        
+
         License: MIT
         """
-        # Remove punctutation from source string.
-        return self.source.translate(str.maketrans('', '', string.punctuation))  
+        if self.ignore_words == True:
+            
+            newstr = self.blank_replacement + self.source.replace(self.delimiter,self.blank_replacement) + self.blank_replacement
+            resl = []
+            for i in range(len(newstr)):
+                resl.append(newstr[:self.ngramcount])
+                newstr = newstr[1:]
+
+            resl.pop()
+            return resl
+
+
+        else:
+            self.words = self.source.split(self.delimiter)
+
+            # Result should be [[list1],[list2],[list3]]
+            # Result list.
+            ngramr = []
+            # Sublists.
+            ngramg = []
+            counter = 0
+
+            while counter < len(self.words):
+                # Append word to sublist.
+                ngramg.append(self.words[counter])
+                if len(ngramg) == self.ngramcount:
+                    # Append sublist to result list.
+                    ngramr.append(self.blank_replacement.join(ngramg))
+                    # Remove first word from sublist.
+                    ngramg = ngramg[1:]
+                counter += 1
+
+            return ngramr
