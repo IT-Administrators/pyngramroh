efa764065363387daff9843ac8d5b3f8404696b5
IT-Administrators <91905626+IT-Administrators@users.noreply.github.com>
2025-12-19 14:28:47 +0100

    Created README
    Created a package README.

--- src/pyngramroh/__init__.py
+++ src/pyngramroh/__init__.py
@@ -5,15 +5,15 @@ A cross plattform ngram module.
 
 Usage:
     from pyngramroh import *
-    ng = NGram("Demo,2,True)
+    ng = NGram("Demo",2,True)
     ng.create_ngram()
     
 Returns:
     ['_D', 'De', 'em', 'mo', 'o_']
 
 Author: IT-Administrators
-License: MIT
 
+License: MIT
 """
 # Functions an classes that can be imported by calling from <modulename> import *
 from .ngram import NGram
